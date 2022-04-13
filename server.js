bf7782945945860797e6c3aafd6e87e8445078f4
Luiz Claro <lfclaro@gmail.com>
2022-04-13 15:43:59 -0400

    testing all routes

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv').config();
 
-// const userRouter = require('./routes/userRoutes'); //responsible to user register
-// const authRouter = require('./routes/authRoutes'); //responsible authorize user
-// const faqRouter = require('./routes/faqRoutes'); //responsible for faq
-// const checkoutRoute = require('./routes/checkoutRoutes'); // get access to our checkout route
-// const contactRoute = require('./routes/contactRoutes'); // get access to our contact route
-// const communityReplyRoute = require('./routes/communityReplyRoute');// get access to our community route
-// const communityPostRoute = require('./routes/communityPostRoute');// get access to our community route
-// const profileRoute = require('./routes/profileRoute');// get access to our profile route
-// const itemRoute = require('./routes/itemRoutes'); // access item route
-// const messageRoute = require('./routes/messageRoute'); // access message route
-// const rentalRoute = require('./routes/rentalRoutes'); // access rental route
-// const adminRoute = require('./routes/adminRoutes'); //access to admin panel
+const userRouter = require('./routes/userRoutes'); //responsible to user register
+const authRouter = require('./routes/authRoutes'); //responsible authorize user
+const faqRouter = require('./routes/faqRoutes'); //responsible for faq
+const checkoutRoute = require('./routes/checkoutRoutes'); // get access to our checkout route
+const contactRoute = require('./routes/contactRoutes'); // get access to our contact route
+const communityReplyRoute = require('./routes/communityReplyRoute');// get access to our community route
+const communityPostRoute = require('./routes/communityPostRoute');// get access to our community route
+const profileRoute = require('./routes/profileRoute');// get access to our profile route
+const itemRoute = require('./routes/itemRoutes'); // access item route
+const messageRoute = require('./routes/messageRoute'); // access message route
+const rentalRoute = require('./routes/rentalRoutes'); // access rental route
+const adminRoute = require('./routes/adminRoutes'); //access to admin panel
 
 connectDB(); //Datebase Connected log should show in console
 
@@ -33,18 +33,18 @@ app.get("/", (req, res) => {
     }
 });
 
-// app.use('/api/users', userRouter);
-// app.use('/api/auth', authRouter);
-// app.use('/api/faqs', faqRouter);
-// app.use('/api/checkout', checkoutRoute);
-// app.use('/api/contact', contactRoute);
-// app.use('/api/communityReply', communityReplyRoute);
-// app.use('/api/communityPost', communityPostRoute);
-// app.use('/api/profile', profileRoute);
-// app.use('/api/items', itemRoute);
-// app.use('/api/messages', messageRoute);
-// app.use('/api/rentals', rentalRoute);
-// app.use('api/admin', adminRoute);
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/faqs', faqRouter);
+app.use('/api/checkout', checkoutRoute);
+app.use('/api/contact', contactRoute);
+app.use('/api/communityReply', communityReplyRoute);
+app.use('/api/communityPost', communityPostRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/items', itemRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/rentals', rentalRoute);
+app.use('api/admin', adminRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
